b01a2302b770c5f7ef10b68d2f4b9eebe0ccf107
ABHISHEK-ROSHAN-000 <abhishekroshan689@gmail.com>
2026-05-19 15:10:51 +0530

    fix: remove trailing slash from assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: "/Reviso-landingPage",
-  assetPrefix: "/Reviso-landingPage/",
+  assetPrefix: "/Reviso-landingPage",
   images: {
     unoptimized: true,
     formats: ['image/webp', 'image/avif'],
