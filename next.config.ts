851ea50c68e118caca1cabe9a9144ac79cf80d59
ABHISHEK-ROSHAN-000 <abhishekroshan689@gmail.com>
2026-05-19 14:55:22 +0530

    added basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: "/Reviso-landingPage",
+  assetPrefix: "/Reviso-landingPage/",
   images: {
     unoptimized: true,
     formats: ['image/webp', 'image/avif'],
