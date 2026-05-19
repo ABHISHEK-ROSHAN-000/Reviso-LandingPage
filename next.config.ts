3d742adb9b1450e78bd9c782f1880d5863b09327
ABHISHEK-ROSHAN-000 <abhishekroshan689@gmail.com>
2026-05-19 14:33:10 +0530

    made static

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
